440ef1c640e210788d3c55c075043bf587f8fa2c
Lewis Booth <lewis@lewisbooth.co.uk>
2017-06-15 16:36:10 +0100

    Add/edit stores now working

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That filetype isn\'t allowed'}, false);
+    }
+  }
+}
 
 exports.homePage = (req, res) => {
   res.render('index');
@@ -7,6 +22,23 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' })
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  if (!req.file) {
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+
+  next();
 };
 
 exports.createStore = async (req, res) => {
@@ -19,7 +51,7 @@ exports.getStores = async (req, res) => {
   // Query database for list of stores
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
-}
+};
 
 exports.editStore = async (req, res) => {
   // Find store given an ID
@@ -28,7 +60,7 @@ exports.editStore = async (req, res) => {
 
   // Render the edit form
   res.render('editStore', { title: `Edit ${store.name}`, store })
-}
+};
 
 exports.updateStore = async (req, res) => {
   // Set location data to be a point
@@ -44,4 +76,19 @@ exports.updateStore = async (req, res) => {
     req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}"> View Store</a>`)
     res.redirect(`/stores/${store.id}/edit`)
   // Redirect to store and tell user it worked
-}
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next();
+  res.render('store', { store, title: store.name })
+};
+
+exports.getStoresByTag = async (req, res) => {
+  const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true };
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+  res.render('tag', { tags, title: 'Tags', tag, stores });
+};
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,11 +3,48 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', storeController.getStores);
-router.get('/stores', storeController.getStores);
-router.get('/add', storeController.addStore);
-router.post('/add', catchErrors((storeController.createStore)));
-router.post('/add/:id', catchErrors((storeController.updateStore)));
-router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.get('/', 
+  storeController.getStores
+);
+
+router.get('/stores', 
+  storeController.getStores
+);
+
+router.get('/stores/:id/edit', 
+  catchErrors(storeController.editStore)
+);
+
+router.get('/store/:slug', 
+  catchErrors(storeController.getStoreBySlug)
+);
+
+router.get('/add', 
+  storeController.addStore
+);
+
+router.post('/add', 
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors((storeController.createStore))
+);  
+  
+router.post('/add/:id', 
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors((storeController.updateStore))
+);
+
+router.get('/tags',
+  catchErrors(storeController.getStoresByTag)
+);
+
+router.get('/tags/:tag',
+  catchErrors(storeController.getStoresByTag)
+);
+
+// router.get('/login', 
+//   userController.loginForm
+// );
 
 module.exports = router;
